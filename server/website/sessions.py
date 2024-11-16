7c8cccabb077a8837c9effc2a0902f04deb9d727
Ritvik <ritvik.gupta2205@gmail.com>
2024-11-16 06:36:12 +0000

    make changes to sessions logic

--- server/website/sessions.py
+++ server/website/sessions.py
@@ -6,6 +6,13 @@ from flask_login import login_user, login_required, logout_user, current_user
 from urllib.parse import urlencode, urljoin
 from .auth import token_required
 from flask_socketio import emit 
+import jwt
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = getenv("SECRET_KEY")
 
 sessions = Blueprint('sessions', __name__)
 
@@ -21,7 +28,7 @@ def create_session(current_user_id):
     total = 0
     admin_id = str(current_user_id)
     created_at = str(datetime.datetime.now())    
-    status = "active"
+    status = "inactive"
     participants = [str(current_user_id)]
     session = {
         "session_name": session_name,
@@ -49,14 +56,15 @@ def get_old_sessions(current_user_id):
 
     response_list = []
     for session in sessions:
-        session_data = {
-            "session_name": session["session_name"],
-            "positions": session["session_positions"],
-            "total_for_person": float(calculate_total_for_user(current_user_id, session)),
-            "total": float(session["total"]),
-            "created_at": str(session["created_at"]),
-        }
-        response_list.append(session_data)
+        if session.get("status") == "closed":
+            session_data = {
+                "session_name": session["session_name"],
+                "positions": session["session_positions"],
+                "total_for_person": float(calculate_total_for_user(current_user_id, session)),
+                "total": float(session["total"]),
+                "created_at": str(session["created_at"]),
+            }
+            response_list.append(session_data)
     return jsonify({"sessions_list": response_list}), 200
 
 
@@ -70,14 +78,29 @@ def calculate_total_for_user(user_id, session):
 
 
 @sessions.route('get_session/<session_id>', methods=['GET'])
-@token_required
-def get_session(current_user_id, session_id):    
+def get_session(session_id):    
     session = sessions_collection.find_one({'_id': ObjectId(session_id)})
-
+    current_user_id = request.get_json().get('user_id')
     if session:
+        status = session.get("status")
+        if status == "active":
+            pass
+        else:
+            token = request.headers.get("x-access-token")
+            if not token:
+                return jsonify({"error": "Token is missing"}), 401
+            
+            try:
+                current_user_id = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])["user_id"]
+            except:
+                return jsonify({"error": "Invalid token"}), 401
+            
+            if session["admin_id"] != current_user_id:
+                return jsonify({"error": "Only the session admin can access this session"}), 403
+            
         session_data = {
             "session_name": session["session_name"],
-            "isClosed": session.get("status") == "closed",
+            "status": session.get("status"),
             "positions": session["session_positions"],
             "total_for_person": float(calculate_total_for_user(current_user_id, session)),
             "total": float(session["total"]),
@@ -119,29 +142,20 @@ def join_session():
 def update_session(current_user_id):
     data = request.get_json()
     session_id = data.get("sessionId")
-    position_index = int(data.get("positionIndex"))
-    item_name = data.get("itemName")
-    amount = int(data.get("amount"))
+    updated_session = data.get("session")
+
     session = sessions_collection.find_one({'_id': ObjectId(session_id)})
-    print(session_id)
+
     if session:        
-        positions = session.get("session_positions")
-        if len(positions) == 0:
-            # initialise positions
-            positions = [{"buyer": current_user_id, "item_name": item_name, "price": amount}]            
-        elif position_index < 0 or position_index >= len(positions):
-            return jsonify({"error": "Position not found"}), 404
         
-        positions[position_index]["buyer"] = current_user_id
-        positions[position_index]["item_name"] = item_name
-        positions[position_index]["price"] = amount
-        total = sum([position["price"] for position in positions if position.get("price")])
+        total = sum([position["price"] for position in updated_session["session_positions"] if position.get("price")])
+        updated_session["total"] = total
         sessions_collection.update_one(
             {"_id": ObjectId(session_id)},
-            {"$set": {"session_positions": positions, "total": total}}
+            {"$set": updated_session}
         )
-        socketio.emit("session_updated", {"session_id": session_id, "positions": positions, "total": total})
-        return jsonify({"message": "Session updated successfully"}), 200
+        socketio.emit("session_updated", {"session_id": session_id, "positions": updated_session["session_positions"], "total": total})
+        return jsonify({"message": "Session updated successfully", "new_total": total}), 200
     else:
         return jsonify({"error": "Session not found"}), 404
 
@@ -179,6 +193,43 @@ def create_link(current_user_id, session_id):
     else:
         return jsonify({"error": "Session not found"}), 404
     
+
+@sessions.route('/confirm_bill/<session_id>', methods=['PUT'])
+@token_required
+def confirm_bill(current_user_id, session_id):
+    session = sessions_collection.find_one({'_id': ObjectId(session_id)})
+
+    if session:
+        if current_user_id == session['admin_id']:
+            sessions_collection.update_one(
+                {'_id': ObjectId(session_id)},
+                {'$set': {'status': 'active'}}
+            )
+            return jsonify({"message": "Bill confirmed successfully"}), 200
+        else:
+            return jsonify({"error": "Only the session admin can confirm the bill"}), 403
+    else:
+        return jsonify({"error": "Session not found"}), 404
+
+
+@sessions.route('/close_session/<session_id>', methods=['PUT'])
+@token_required
+def close_session(current_user_id, session_id):
+    session = sessions_collection.find_one({'_id': ObjectId(session_id)})
+
+    if session:
+        if current_user_id == session['admin_id']:
+            sessions_collection.update_one(
+                {'_id': ObjectId(session_id)},
+                {'$set': {'status': 'closed'}}
+            )
+            return jsonify({"message": "Session closed successfully"}), 200
+        else:
+            return jsonify({"error": "Only the session admin can close the session"}), 403
+    else:
+        return jsonify({"error": "Session not found"}), 404
+    
+
 
 @sessions.route('join_link/<session_id>', methods=['GET'])
 def join_link(session_id):
